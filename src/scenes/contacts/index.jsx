5018185a10a4eca8d591e117cda836637cb7db56
IsmaelAsimbaya <85204681+IsmaelAsimbaya@users.noreply.github.com>
2023-07-28 18:22:26 -0500

    Listar Pacientes

--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -1,53 +1,94 @@
+import { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import { mockDataContacts } from "../../data/mockData";
+//import { mockDataContacts } from "../../data/mockData";
 import Header from "../../components/Header";
 import { useTheme } from "@mui/material";
+import axios from "axios"; // Importa la librería axios para realizar la solicitud HTTP
+
 
 const Contacts = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [rows, setRows] = useState([]); // Estado para almacenar los datos obtenidos desde la API
+
+  useEffect(() => {
+    // Utiliza el hook useEffect para realizar la solicitud a la API al cargar el componente
+    axios
+      .get("http://localhost:9090/pacientes") // Reemplaza "http://ruta-de-tu-api.com/pacientes" con la URL de tu API
+      .then((response) => {
+        // Asigna un id único a cada fila antes de actualizar el estado
+        const rowsWithId = response.data.map((row, index) => ({
+          ...row,
+          id: index + 1, // Puedes utilizar algún campo único de tus datos para obtener un id más adecuado
+        }));
+        // Cuando se obtienen los datos de la API con éxito
+        //setRows(response.data); // Actualiza el estado con los datos de la API
+        setRows(rowsWithId);
+      })
+      .catch((error) => {
+        // Manejo de errores en caso de que la solicitud falle
+        console.error("Error al obtener datos desde la API:", error);
+      });
+  }, []);
 
   const columns = [
-    { field: "id", headerName: "ID", flex: 0.5 },
-    { field: "registrarId", headerName: "Registrar ID" },
+    { 
+      field: "id_pac", 
+      headerName: "ID", 
+      flex: 0.5 
+    },
+    { 
+      field: "cedula_pac", 
+      headerName: "Cedula" 
+    },
     {
-      field: "name",
-      headerName: "Name",
+      field: "nombre_pac",
+      headerName: "Nombre",
       flex: 1,
       cellClassName: "name-column--cell",
     },
     {
-      field: "age",
-      headerName: "Age",
+      field: "apellido_paterno_pac",
+      headerName: "Apellido Paterno",
+      flex: 1,
+    },
+    {
+      field: "apellido_materno_pac",
+      headerName: "Apellido Materno",
+      flex: 1,
+    },
+    {
+      field: "sexo_pac",
+      headerName: "Sexo",
       type: "number",
       headerAlign: "left",
       align: "left",
     },
     {
-      field: "phone",
-      headerName: "Phone Number",
+      field: "fecha_nac_pac",
+      headerName: "Fecha Nacimiento",
       flex: 1,
     },
     {
-      field: "email",
-      headerName: "Email",
+      field: "domicilio_pac",
+      headerName: "Domicilio",
       flex: 1,
     },
     {
-      field: "address",
-      headerName: "Address",
+      field: "telefono_pac",
+      headerName: "Telefono",
       flex: 1,
     },
     {
-      field: "city",
-      headerName: "City",
+      field: "num_expediente_pac",
+      headerName: "N.Expediente",
       flex: 1,
     },
     {
-      field: "zipCode",
-      headerName: "Zip Code",
+      field: "id_hospitalario_pac",
+      headerName: "ID.Hospitalario",
       flex: 1,
     },
   ];
@@ -55,8 +96,8 @@ const Contacts = () => {
   return (
     <Box m="20px">
       <Header
-        title="CONTACTS"
-        subtitle="List of Contacts for Future Reference"
+        title="PACIENTES"
+        subtitle="Lista de pacientes registrados"
       />
       <Box
         m="40px 0 0 0"
@@ -91,9 +132,10 @@ const Contacts = () => {
         }}
       >
         <DataGrid
-          rows={mockDataContacts}
+          rows={rows} // Reemplaza "mockDataContacts" con "rows"
           columns={columns}
           components={{ Toolbar: GridToolbar }}
+          getRowId={(row) => row.id} // Especifica la función para obtener el id de cada fila
         />
       </Box>
     </Box>
