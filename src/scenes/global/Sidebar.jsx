defb9b0b02bc91a83232511fe6b2401ce70f19c2
IsmaelAsimbaya <85204681+IsmaelAsimbaya@users.noreply.github.com>
2023-07-31 11:32:18 -0500

    profile pic

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -96,7 +96,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src={`../../assets/crud.png`}
+                  src={`../../assets/sim.png`}
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
